fed6e2ce11a707dec3586fb0074898f5b9b113ee
code wizard <allanmaaz@Allans-MacBook-Pro.local>
2024-11-25 22:46:13 +0530

    variable test

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello and welcome!");
-        System.out.println("    Hello and welcome!");
-
-
-
+int x;//declaretion
+x=345;//assignment
+        System.out.println(x);//printing of the variable
     }
 }
